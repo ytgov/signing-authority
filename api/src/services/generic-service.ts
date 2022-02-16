c5a173fc91d9182af33564e8f02b63cc8a2b4750
Ryan Agar <ryanjagar@hey.com>
2022-02-15 21:42:58 -0700

    refactored for mongodriver v4

--- api/src/services/generic-service.ts
+++ api/src/services/generic-service.ts
@@ -1,5 +1,6 @@
 // import { Collection, FilterQuery, ObjectId } from "mongodb";
-import { Collection, ObjectId } from "mongodb";
+import { Collection, ObjectId, Filter } from "mongodb";
+import { API_GATEWAY_KEY } from "src/config";
 
 export class GenericService {
 
@@ -10,6 +11,7 @@ export class GenericService {
     }
 
     async create(item: any): Promise<any> {
+        console.log(item)
         return this.db.insertOne(item);
     }
 
@@ -21,13 +23,13 @@ export class GenericService {
         return this.db.deleteOne({ _id: new ObjectId(id) });
     }
 
-    // async deleteWhere(query: FilterQuery<any>): Promise<any> {
-    //     return this.db.deleteMany(query);
-    // }
+    async getAll(query: Filter<any>, sort?: any): Promise<any[]> {
+        return this.db.find(query).sort(sort).toArray();
+    }
 
-    // async getAll(query?: FilterQuery<any>, sort?: any): Promise<any[]> {
-    //     return this.db.find(query).sort(sort).toArray();
-    // }
+    async deleteWhere(query: Filter<any>): Promise<any> {
+        return this.db.deleteMany(query);
+    }
 
     async getById(id: string): Promise<any | null> {
         try {
