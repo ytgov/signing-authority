5b17f3f58a3d3b854733296bb489f8c9b65246ce
Ryan Agar <ryanjagar@hey.com>
2022-03-10 11:25:39 -0700

    adding some search metadata

--- api/src/data/models/file.ts
+++ api/src/data/models/file.ts
@@ -5,4 +5,8 @@ export interface StoredFile {
     fileSize: number;
     mimeType: string;
     content: Buffer;
+    uploadedBy?: string; //the user who uploaded this file
+    user?: string; //the User for whom this FormB is for
+    authorityID?: string; //the ID of the SigningAuth this relates to
+    inEffect?: boolean; //a flag if this is the currenent SA (may be better tracked in the actual authority doc)
 }
