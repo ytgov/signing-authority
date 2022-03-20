233d7b4239a099dd441b096a1bf5770b3a26cf69
Michael Johnson <michael@icefoganalytics.com>
2022-03-20 10:37:41 -0700

    Employee pages changes

---
+++ web/src/components/AuthorityRenderer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-card class="default mb-4" to="/detail">
+      <v-card-text>
+        <p>Department: {{ authority.department.name }}</p>
+        <p>Program: {{ authority.program }}</p>
+        <p>Title: {{ authority.title }}</p>
+        <p>Issue date: {{ authority.issue_date }}</p>
+        <p>Lines: {{ authority.authority_lines.length }}</p>
+
+        <v-btn color="error" class="mr-5">Archive</v-btn>
+        <v-btn color="info" class="mr-5">Make changes</v-btn>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthorityRenderer",
+  props: ["authority"],
+};
+</script>
--- web/src/views/Employee/Detail.vue
+++ web/src/views/Employee/Detail.vue
@@ -1,44 +1,50 @@
 <template>
   <div>
-    <v-row>
-      <v-col>
     <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
-      </v-col>
-    </v-row>
-    <v-row>
-    <v-col>
+
     <h1>
       {{ employee.first_name }} {{ employee.last_name }}
       <small> ({{ employee.ynet_id }})</small>
     </h1>
-    </v-col>
-    <v-spacer></v-spacer>
-  <v-col>
-     <form-b-upload-modal> </form-b-upload-modal>
-  </v-col>
-    </v-row>
-    <h3>Employee Id: {{ employee.employee_id }}</h3>
 
-    <strong>Authorities:</strong>
-    {{ employee.authorities }}
+    <v-row>
+      <v-col cols="12" sm="6">
+        <h3>Employee Id: {{ employee.employee_id }}</h3>
+        <p>First name: {{ employee.first_name }}</p>
+        <p>Last name: {{ employee.last_name }}</p>
+        <p>YNET Id: {{ employee.ynet_id }}</p>
+
+        <form-b-upload-modal> </form-b-upload-modal>
+      </v-col>
+
+      <v-col cols="12" sm="6">
+        <h3>Authorities:</h3>
+
+        <div v-for="(item, idx) of employee.authorities" :key="idx">
+          <AuthorityRenderer :authority="item"></AuthorityRenderer>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import formBUploadModal from "../../components/forms/formBUploadModal.vue"
+import formBUploadModal from "../../components/forms/formBUploadModal.vue";
+import AuthorityRenderer from "../../components/AuthorityRenderer.vue";
 //import store from "../../store";
 
 export default {
-  components: { formBUploadModal },
+  components: { formBUploadModal, AuthorityRenderer },
   name: "Login",
   component: {
-    formBUploadModal
+    formBUploadModal,
+    AuthorityRenderer,
   },
   computed: {
     ...mapGetters("employee", ["employee"]),
     breadcrumbs: function () {
-      let b = [{ text: "Employees", to: "/employee" }];
+      let b = [{ text: "Dashboard", to: "/dashboard" }];
       b.push({
         text: `${this.employee.first_name} ${this.employee.last_name}`,
       });
