f1bf7729284277c9413ac20b245ad416c1da3595
Michael Johnson <michael@icefoganalytics.com>
2022-03-20 10:14:59 -0700

    Clean up

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -43,7 +43,7 @@
                 Advanced search</v-btn
               >
             </div>
-            <div class="float-left">Authority ({{ resultCount }} matches)</div>
+            <div class="float-left">Employees ({{ searchResults.length }} matches)</div>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
@@ -54,61 +54,24 @@
         <v-data-table
           hide-default-footer
           :headers="[
-            { text: '', value: 'action', width: '40px' },
             { text: 'Name', value: 'display_name' },
             { text: 'Title', value: 'title' },
             { text: 'Department', value: 'department' },
           ]"
           :items="searchResults"
           :items-per-page="-1"
-          @click:row="selectAuthority"
+          :loading="loading"
+          @click:row="selectEmployee"
           class="clickable-row"
         >
-          <template v-slot:item.action="{ item }">
-            <v-btn
-              outlined
-              icon
-              color="primary"
-              :to="`/authority/${item.student_id}`"
-              title="View student record"
-              ><v-icon>mdi-signature</v-icon></v-btn
-            >
-          </template>
         </v-data-table>
       </div>
-
-      <v-divider></v-divider>
-
-      <v-card class="default my-4 mx-5" v-if="selectedAuthority">
-        <v-card-title>Authority for {{ selectedAuthority.name }}</v-card-title>
-        <v-card-text>
-          <div v-for="(app, i) of signingAuthorities" :key="i">
-            <v-list-item
-              two-line
-              :to="'/card/' + app.HISTORY_DETAIL_ID + '/personal'"
-            >
-              <v-list-item-content>
-                <v-list-item-title
-                  >{{ app.ACADEMIC_YEAR }}:
-                  {{ app.institution_name }}</v-list-item-title
-                >
-                <v-list-item-subtitle
-                  >{{ app.study_area_name }} ({{
-                    app.program_name
-                  }})<br />Applications: YG</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-            <v-divider v-if="i < signingAuthorities.length - 1"></v-divider>
-          </div>
-        </v-card-text>
-      </v-card>
     </v-navigation-drawer>
   </div>
 </template>
 
 <script>
-import {EMPLOYEE_URL} from "../urls"
+import { EMPLOYEE_URL } from "../urls";
 import axios from "axios";
 
 export default {
@@ -116,24 +79,18 @@ export default {
   data: () => ({
     search: "",
     drawer: null,
-    selectedAuthority: null,
-    signingAuthorities: [],
     searchResults: [],
-    resultCount: 0,
+    loading: false,
   }),
   methods: {
     searchKeyUp(event) {
       if (event.key == "Enter") this.doSearch();
     },
     doSearch() {
-      this.selectedAuthority = null;
-      this.signingAuthorities = [];
       let cleanSearch = this.search.trim().toLowerCase();
       if (cleanSearch.length == 0) return;
-      // hack to show search funtcion
-      this.searchResults = [{ name: "Jane Smith" }, { name: "Alex Jones" }];
-      this.drawer = true;
-      this.resultCount = this.searchResults.length;
+
+      this.loading = true;
 
       axios
         .post(`${EMPLOYEE_URL}/search`, { terms: cleanSearch })
@@ -144,10 +101,12 @@ export default {
         })
         .catch((err) => {
           this.$emit("showError", err);
+        })
+        .finally(() => {
+          this.loading = false;
         });
     },
-    selectAuthority(item) {
-      this.selectedAuthority = item;
+    selectEmployee(item) {
       this.$router.push(`/employee/${item._id}`);
     },
   },
