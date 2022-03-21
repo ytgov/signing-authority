6b6768878fd70a188215cd548e7d2f5e8fb4b16f
Michael Johnson <michael@icefoganalytics.com>
2022-03-21 10:53:11 -0700

    Table formatting

--- web/src/views/AuthorityDetails.vue
+++ web/src/views/AuthorityDetails.vue
@@ -2,16 +2,100 @@
 <template>
   <div>
     <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
+
+    <h1>
+      Form B for {{ formB.employee.first_name }} {{ formB.employee.last_name }}
+    </h1>
+
     <v-row>
       <v-col>
         <v-card class="default">
           <v-card-title> Future Home of Viewing a Signing Auth</v-card-title>
-          <v-card-text> 
-            <v-data-table
+          <v-card-text>
+            <table
+              border="0"
+              cellspacing="0"
+              cellpadding="0"
+              class="table"
+              style="background-color: white; width: 100%; text-align: left"
+            >
+              <thead>
+                <tr>
+                  <th rowspan="5"></th>
+                  <th colspan="13">SPENDING AUTHORITY</th>
+                  <th rowspan="3" class="rotate" style="height: 140px">
+                    <div>PAYMENT<br> AUTHORITY s.30</div>
+                  </th>
+                </tr>
+                <tr>
+                  <th colspan="6">PROCUREMENT</th>
+                  <th rowspan="2" colspan="2" style="max-width: 80px">
+                    TRANSFER PAYMENTS
+                  </th>
+                  <th colspan="4" rowspan="2">OTHER:<br>S.24 COMMITMENT & <br>S.23 SIGNING CONTRACT</th>
+                  <th rowspan="2">S.29</th>
+                </tr>
+                <tr>
+                  <th colspan="4">S.24 Comitment</th>
+                  <th colspan="2" style="max-width: 80px">
+                    S.23 Signing Contract
+                  </th>
+                </tr>
+                <tr>
+                  <th colspan="2" style="max-width: 80px">OWN PROCUREMENT</th>
+                  <th rowspan="2" class="rotate">
+                    <div>Request for goods</div>
+                  </th>
+                  <th rowspan="2" class="rotate"><div>Assignent Auth</div></th>
+                  <th rowspan="2" class="rotate"><div>Goods</div></th>
+                  <th rowspan="2" class="rotate"><div>Services</div></th>
+                  <th rowspan="2" class="rotate"><div>S.24 Commitment</div></th>
+                  <th rowspan="2" class="rotate">
+                    <div>S.23 Signing contract</div>
+                  </th>
+                  <th rowspan="2" class="rotate">
+                    <div>S.24 Travel auth</div>
+                  </th>
+                  <th rowspan="2" class="rotate"><div>OTher contrat</div></th>
+                  <th rowspan="2" class="rotate"><div>Loans</div></th>
+                  <th rowspan="2" class="rotate"><div>Departmental</div></th>
+                  <th rowspan="2" class="rotate"><div>Cert of perform</div></th>
+                  <th rowspan="2" class="rotate">
+                    <div>Requesition for payment</div>
+                  </th>
+                </tr>
+                <tr>
+                  <th class="rotate"><div>Goods</div></th>
+                  <th class="rotate"><div>Services</div></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(line, idx) of formB.authority_lines" :key="idx">
+                  <td>{{ line.account }}</td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                  <td style="width: 60px"></td>
+                </tr>
+              </tbody>
+            </table>
+
+            <!--   <v-data-table style="font-size: .5rem !important"
+            dense
             :headers="[{text:'Account', value:'account'}, {text: 'S23 Goods', value:'s24_procure_goods_limit'}]"
             :items="formB.authority_lines"></v-data-table>
 
-
+-->
           </v-card-text>
           <v-card-actions>
             <v-btn color="primary" class="mx-5">
@@ -28,8 +112,34 @@
   </div>
 </template>
 
+<style scoped>
+.table {
+  border-collapse: collapse;
+}
+.table th {
+  text-align: center;
+}
+.table th,
+.table td {
+  border: 1px black solid;
+}
+
+.table th.rotate {
+  height: 170px;
+  white-space: nowrap;
+  vertical-align: bottom;
+  padding-bottom: 20px;
+}
+
+.table th.rotate > div {
+  transform: rotate(270deg);
+  width: 58px;
+}
+</style>
+
+
 <script>
-import { mapGetters, mapActions} from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "AuthorityDetails",
@@ -42,8 +152,8 @@ export default {
     breadcrumbs: function () {
       let b = [{ text: "Dashboard", to: "/dashboard" }];
       b.push({
-         text: `${this.formB.employee.first_name} ${this.formB.employee.last_name}`,
-         to: `/employee/${this.formB.employee_id}`
+        text: `${this.formB.employee.first_name} ${this.formB.employee.last_name}`,
+        to: `/employee/${this.formB.employee_id}`,
       });
       b.push({ text: `Form B (${this.formB.department.name} - )` });
       return b;
@@ -55,6 +165,6 @@ export default {
   },
   methods: {
     ...mapActions("authority", ["loadFormB"]),
-  }
+  },
 };
 </script>
