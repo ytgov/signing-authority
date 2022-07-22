006217da3f80db59eaff2d3a8fd4a30b08a4f6ad
Michael Johnson <michael@icefoganalytics.com>
2022-07-21 17:53:30 -0700

    Form B Changes

--- web/src/modules/forms/formB/views/AuthorityDetails.vue
+++ web/src/modules/forms/formB/views/AuthorityDetails.vue
@@ -1,229 +1,236 @@
 
 <template>
-  <div>
-    <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
+  <v-container fluid class="down-top-padding">
+    <BaseBreadcrumb
+      :title="page.title"
+      :icon="page.icon"
+      :breadcrumbs="breadcrumbs"
+    >
+      <template v-slot:right>
+        <!-- <timed-message ref="messager" class="mr-4"></timed-message> -->
+      </template>
+    </BaseBreadcrumb>
 
-    <div class="float-right">
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="secondary" small v-bind="attrs" v-on="on" class="mt-2">
-            Actions <v-icon>mdi-chevron-down</v-icon>
-          </v-btn>
-        </template>
-        <v-list dense>
-          <v-list-item @click="editClick">
-            <v-list-item-title>Edit</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item @click="generateClick">
-            <v-list-item-title>Lock for Signatures</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item @click="uploadClick">
-            <upload-form-modal></upload-form-modal>
-            <!-- <v-list-item-title>Upload Signed PDF</v-list-item-title> -->
-          </v-list-item>
-
-          <v-list-item @click="archiveClick">
-            <v-list-item-title>Archive</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item @click="archiveClick">
-            <v-list-item-title>Duplicate</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-    <h1>
-      Form B for {{ formB.employee.first_name }}
-      {{ formB.employee.last_name }}
-    </h1>
-
-    <authority-metadata-card :formB="formB" />
-
-    <v-row>
-      <v-col>
-        <v-card class="default">
-          <v-card-text>
-            <table
-              border="0"
-              cellspacing="0"
-              cellpadding="0"
-              class="table"
-              style="background-color: white; width: 100%; text-align: left"
-            >
-              <thead>
-                <tr>
-                  <th
-                    rowspan="5"
-                    style="text-align: left; padding: 10px; vertical-align: top"
-                  >
-                    <h3>Delegate:</h3>
-                    Public Officer Name:<br />
-                    <strong>
-                      {{ formB.employee.first_name }}
-                      {{ formB.employee.last_name }}
-                    </strong>
-                    <br /><br />
-
-                    Department:<br />
-                    <strong>{{ formB.department_desc }}</strong>
-                    <br /><br />
-                    Program/Branch:<br />
-                    <strong>{{ formB.program }}</strong>
-                    <br /><br />
-                    Position title:<br />
-                    <strong>{{ formB.title }}</strong>
-                  </th>
-                  <th colspan="13">SPENDING AUTHORITY</th>
-                  <th rowspan="3" class="rotate" style="height: 140px">
-                    <div>
-                      PAYMENT<br />
-                      AUTHORITY s.30
-                    </div>
-                  </th>
-                </tr>
-                <tr>
-                  <th colspan="6">PROCUREMENT</th>
-                  <th rowspan="2" colspan="2" style="max-width: 80px">
-                    TRANSFER PAYMENTS
-                  </th>
-                  <th colspan="4" rowspan="2">
-                    OTHER:<br />S.24 COMMITMENT & <br />S.23 SIGNING CONTRACT
-                  </th>
-                  <th rowspan="2">S.29</th>
-                </tr>
-                <tr>
-                  <th colspan="4">S.24 Commitment</th>
-                  <th colspan="2" style="max-width: 80px">
-                    S.23 Signing Contract
-                  </th>
-                </tr>
-                <tr>
-                  <th colspan="2" style="max-width: 80px">OWN PROCUREMENT</th>
-                  <th rowspan="3" class="rotate">
-                    <div>
-                      Request for goods<br />
-                      or services
-                    </div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>Assignment <br />Authority</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div class="mb-2">Goods</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div class="mb-2">Services</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div class="mb-2">S.24 Commitment</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>S.23 Signing<br />contract</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>S.24 Travel <br />authorization</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>OTher contracts & <br />Expenditures **</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>Loans & <br />Guarantees</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div class="mb-2">Departmental Use</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>Certificate of <br />performance</div>
-                  </th>
-                  <th rowspan="3" class="rotate">
-                    <div>
-                      Requesition<br />
-                      for payment
-                    </div>
-                  </th>
-                </tr>
-                <tr>
-                  <th class="rotate" rowspan="2">
-                    <div class="mb-2">Goods</div>
-                  </th>
-                  <th class="rotate" rowspan="2">
-                    <div class="mb-2">Services</div>
-                  </th>
-                </tr>
-                <tr style="height: 30px">
-                  <th style="height: 30px">
-                    Area of Authority - General Ledger Coding
-                  </th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr v-for="(line, idx) of formB.authority_lines" :key="idx">
-                  <td class="pl-3">{{ line.account }}</td>
-                  <td class="fb-value">{{ line.s24_procure_goods_limit }}</td>
-                  <td class="fb-value">
-                    {{ line.s24_procure_services_limit }}
-                  </td>
-                  <td class="fb-value">
-                    {{ line.s24_procure_request_limit }}
-                  </td>
-                  <td class="fb-value">
-                    {{ line.s24_procure_assignment_limit }}
-                  </td>
-                  <td class="fb-value">
-                    {{ line.s23_procure_goods_limit }}
-                  </td>
-                  <td class="fb-value">
-                    {{ line.s23_procure_services_limit }}
-                  </td>
-                  <td class="fb-value">{{ line.s24_transfer_limit }}</td>
-                  <td class="fb-value">{{ line.s23_transfer_limit }}</td>
-                  <td class="fb-value">{{ line.s24_travel_limit }}</td>
-                  <td class="fb-value">{{ line.other_limit }}</td>
-                  <td class="fb-value">{{ line.loans_limit }}</td>
-                  <td class="fb-value">{{ line.trust_limit }}</td>
-                  <td class="fb-value">{{ line.s29_performance_limit }}</td>
-                  <td class="fb-value">{{ line.s30_payment_limit }}</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <!--   <v-data-table style="font-size: .5rem !important"
-            dense
-            :headers="[{text:'Account', value:'account'}, {text: 'S23 Goods', value:'s24_procure_goods_limit'}]"
-            :items="formB.authority_lines"></v-data-table>
-
--->
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
+    <BaseCard
+      showHeader="true"
+      :heading="`Form B for ${formB.employee.first_name} ${formB.employee.last_name}`"
+    >
+      <template slot="right">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
             <v-btn
-              :to="{
-                name: 'EmployeeDetail',
-                params: { id: formB.employee._id },
-              }"
-              color="#7A9A01"
-              >Close</v-btn
+              color="secondary"
+              small
+              v-bind="attrs"
+              v-on="on"
+              class="mt-2"
             >
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+              Actions <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list dense>
+            <v-list-item @click="editClick">
+              <v-list-item-title>Edit</v-list-item-title>
+            </v-list-item>
 
-    <v-dialog></v-dialog>
-  </div>
+            <v-list-item @click="generateClick">
+              <v-list-item-title>Lock for Signatures</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item @click="uploadClick">
+              <upload-form-modal></upload-form-modal>
+              <!-- <v-list-item-title>Upload Signed PDF</v-list-item-title> -->
+            </v-list-item>
+
+            <v-list-item @click="archiveClick">
+              <v-list-item-title>Archive</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item @click="archiveClick">
+              <v-list-item-title>Duplicate</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+
+      <authority-metadata-card :formB="formB" />
+
+      <v-row>
+        <v-col>
+          <v-card class="default">
+            <v-card-text>
+              <table
+                border="0"
+                cellspacing="0"
+                cellpadding="0"
+                class="table"
+                style="background-color: white; width: 100%; text-align: left"
+              >
+                <thead>
+                  <tr>
+                    <th
+                      rowspan="5"
+                      style="
+                        text-align: left;
+                        padding: 10px;
+                        vertical-align: top;
+                      "
+                    >
+                      <h3>Delegate:</h3>
+                      Public Officer Name:<br />
+                      <strong>
+                        {{ formB.employee.first_name }}
+                        {{ formB.employee.last_name }}
+                      </strong>
+                      <br /><br />
+
+                      Department:<br />
+                      <strong>{{ formB.department_desc }}</strong>
+                      <br /><br />
+                      Program/Branch:<br />
+                      <strong>{{ formB.program }}</strong>
+                      <br /><br />
+                      Position title:<br />
+                      <strong>{{ formB.title }}</strong>
+                    </th>
+                    <th colspan="13">SPENDING AUTHORITY</th>
+                    <th rowspan="3" class="rotate" style="height: 140px">
+                      <div>
+                        PAYMENT<br />
+                        AUTHORITY s.30
+                      </div>
+                    </th>
+                  </tr>
+                  <tr>
+                    <th colspan="6">PROCUREMENT</th>
+                    <th rowspan="2" colspan="2" style="max-width: 80px">
+                      TRANSFER PAYMENTS
+                    </th>
+                    <th colspan="4" rowspan="2">
+                      OTHER:<br />S.24 COMMITMENT & <br />S.23 SIGNING CONTRACT
+                    </th>
+                    <th rowspan="2">S.29</th>
+                  </tr>
+                  <tr>
+                    <th colspan="4">S.24 Commitment</th>
+                    <th colspan="2" style="max-width: 80px">
+                      S.23 Signing Contract
+                    </th>
+                  </tr>
+                  <tr>
+                    <th colspan="2" style="max-width: 80px">OWN PROCUREMENT</th>
+                    <th rowspan="3" class="rotate">
+                      <div>
+                        Request for goods<br />
+                        or services
+                      </div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>Assignment <br />Authority</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div class="mb-2">Goods</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div class="mb-2">Services</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div class="mb-2">S.24 Commitment</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>S.23 Signing<br />contract</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>S.24 Travel <br />authorization</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>OTher contracts & <br />Expenditures **</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>Loans & <br />Guarantees</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div class="mb-2">Departmental Use</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>Certificate of <br />performance</div>
+                    </th>
+                    <th rowspan="3" class="rotate">
+                      <div>
+                        Requesition<br />
+                        for payment
+                      </div>
+                    </th>
+                  </tr>
+                  <tr>
+                    <th class="rotate" rowspan="2">
+                      <div class="mb-2">Goods</div>
+                    </th>
+                    <th class="rotate" rowspan="2">
+                      <div class="mb-2">Services</div>
+                    </th>
+                  </tr>
+                  <tr style="height: 30px">
+                    <th style="height: 30px">
+                      Area of Authority - General Ledger Coding
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr v-for="(line, idx) of formB.authority_lines" :key="idx">
+                    <td class="pl-3">{{ line.account }}</td>
+                    <td class="fb-value">{{ line.s24_procure_goods_limit }}</td>
+                    <td class="fb-value">
+                      {{ line.s24_procure_services_limit }}
+                    </td>
+                    <td class="fb-value">
+                      {{ line.s24_procure_request_limit }}
+                    </td>
+                    <td class="fb-value">
+                      {{ line.s24_procure_assignment_limit }}
+                    </td>
+                    <td class="fb-value">
+                      {{ line.s23_procure_goods_limit }}
+                    </td>
+                    <td class="fb-value">
+                      {{ line.s23_procure_services_limit }}
+                    </td>
+                    <td class="fb-value">{{ line.s24_transfer_limit }}</td>
+                    <td class="fb-value">{{ line.s23_transfer_limit }}</td>
+                    <td class="fb-value">{{ line.s24_travel_limit }}</td>
+                    <td class="fb-value">{{ line.other_limit }}</td>
+                    <td class="fb-value">{{ line.loans_limit }}</td>
+                    <td class="fb-value">{{ line.trust_limit }}</td>
+                    <td class="fb-value">{{ line.s29_performance_limit }}</td>
+                    <td class="fb-value">{{ line.s30_payment_limit }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                :to="{
+                  name: 'EmployeeDetail',
+                  params: { id: formB.employee._id },
+                }"
+                color="#7A9A01"
+                >Close</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </BaseCard>
+  </v-container>
 </template>
-
-
-
 
 <script>
 import { AUTHORITY_URL } from "@/urls";
 import { mapGetters, mapActions } from "vuex";
 
-import uploadFormModal from "../components/uploadFormModal.vue"
+import uploadFormModal from "../components/uploadFormModal.vue";
 import AuthorityMetadataCard from "../components/authorityMetadataCard.vue";
 
 export default {
@@ -236,6 +243,7 @@ export default {
     id: "",
     authority: {},
     showUpload: false,
+    page: { title: "" },
   }),
   computed: {
     ...mapGetters("authority/formB", ["formB"]),
@@ -247,14 +255,15 @@ export default {
         to: `/employee/${this.formB.employee_id}`,
       });
       b.push({
-        text: `Form B (${this.formB.department_descr} - ${this.formB.program})`,
+        text: `${this.formB.department_descr} / ${this.formB.program} / ${this.formB.title}`,
       });
       return b;
     },
   },
   async mounted() {
-    this.loadFormB(this.$route.params.id);
-    this.id = this.$route.params.id;
+    this.id = this.$route.params.formBId;
+    this.loadFormB(this.id);
+    this.page.title = "Form B Details";
   },
   methods: {
     ...mapActions("authority/formB", ["loadFormB", "downloadFormB"]),
