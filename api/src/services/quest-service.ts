451dc4aa43d854d9315791408d303d5ef2a8e507
Michael Johnson <michael@icefoganalytics.com>
2022-11-09 18:14:29 -0700

    Caching FM results

--- api/src/services/quest-service.ts
+++ api/src/services/quest-service.ts
@@ -6,6 +6,8 @@ const DEPARTMENT_API_URL = "https://api.gov.yk.ca/finance/api/v2/accounts/depart
 const FM_AUTH_HEADER = { "Ocp-Apim-Subscription-Key": FM_ACCOUNT_LOOKUP_KEY };
 
 export class QuestService {
+  cache = new Map();
+
   constructor() {}
 
   async getDepartmentList() {
@@ -23,12 +25,21 @@ export class QuestService {
   async accountPatternIsValid(input: string): Promise<Boolean> {
     // must swap 'x' to underscore and append underscore to end of account to force wildcard search
     input = input.replace(/x/g, "_") + "_";
+    input = input.replace(/-/g, "");
+
+    if (this.cache.has(input)) {
+      return this.cache.get(input);
+    }
 
     return axios
       .get(`${FM_ACCOUNT_LOOKUP_URL}/account/${input}`, { headers: FM_AUTH_HEADER })
       .then(async (resp) => {
         let count = (resp.data.count as Number) || 0;
-        return count > 0;
+        let val = count > 0;
+
+        if (!this.cache.has(input)) this.cache.set(input, val);
+
+        return val;
       })
       .catch((error) => {
         console.log("Error connected to the API Gateway", error);
