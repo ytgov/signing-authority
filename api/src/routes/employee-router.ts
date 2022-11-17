bb8ab2185f8395567dca71b0dd3c0f37b79946b6
Michael Johnson <michael@icefoganalytics.com>
2022-11-16 22:08:02 -0700

    Employee searching

--- api/src/routes/employee-router.ts
+++ api/src/routes/employee-router.ts
@@ -13,49 +13,54 @@ employeeRouter.use(RequiresData, checkJwt);
 
 const directoryService = new DirectoryService();
 
-employeeRouter.get('/', async (req: Request, res: Response) => {
+employeeRouter.get("/", async (req: Request, res: Response) => {
   //return all the authorites assigned to the account
   let db = req.store.Employees as GenericService<Employee>;
 
   return res.json({ data: await db.getAll({}) });
 });
 
-employeeRouter.post('/', async (req: Request, res: Response) => {
+employeeRouter.post("/", async (req: Request, res: Response) => {
   //create a new employee
   let db = req.store.Employees as GenericService<Employee>;
   let created = await db.create(req.body);
   return res.json(created);
 });
 
-employeeRouter.post('/search',
-  [body("terms").notEmpty().trim()], ReturnValidationErrors,
+employeeRouter.post(
+  "/search",
+  [body("term").notEmpty().trim()],
+  ReturnValidationErrors,
   async (req: Request, res: Response) => {
-    let { terms } = req.body;
-
-    //return all the authorites assigned to the account
-
-    let db = req.store.Employees as GenericService<Employee>;
-    let reg = new RegExp(terms, "i");
+    let { term } = req.body;
+    let db = req.store.Authorities as GenericService<Authority>;
+    let reg = new RegExp(term, "i");
 
     let list = await db.getAll({
-      $or: [
-        { "first_name": { $regex: reg } },
-        { "last_name": { $regex: reg } },
-        { "ynet_id": { $regex: reg } },
-      ]
+      $or: [{ "employee.name": { $regex: reg } }, { "employee.ynet_id": { $regex: reg } }],
     });
 
-    //console.log("MATCHES FOR: ", terms, list.length);
+    let results = new Map();
 
     for (let item of list) {
-      item.display_name = `${item.first_name} ${item.last_name}`;
-      item.long_name = `${item.display_name} (${item.ynet_id}) - ${item.position}`;
+      if (results.has(item.employee.ynet_id)) {
+        let rs = results.get(item.employee.ynet_id);
+        rs.authority_count++;
+      } else {
+        results.set(item.employee.ynet_id, {
+          display_name: item.employee.name,
+          ynet_id: item.employee.ynet_id,
+          authority_count: 1,
+        });
+      }
     }
 
-    return res.json({ data: list });
-  });
+    return res.json({ data: Array.from(results.values()) });
+  }
+);
 
-employeeRouter.post('/search-directory',
+employeeRouter.post(
+  "/search-directory",
   //[body("terms").notEmpty().trim()], ReturnValidationErrors,
   async (req: Request, res: Response) => {
     let { terms } = req.body;
@@ -64,69 +69,61 @@ employeeRouter.post('/search-directory',
     let results = await directoryService.search(terms);
 
     return res.json({ data: results });
-  });
+  }
+);
 
-employeeRouter.get('/:id',
-  [param("id").notEmpty()], ReturnValidationErrors,
-  async (req: Request, res: Response) => {
-    let empDb = req.store.Employees as GenericService<Employee>;
-    let autDb = req.store.Authorities as GenericService<Authority>;
+employeeRouter.get("/:id", [param("id").notEmpty()], ReturnValidationErrors, async (req: Request, res: Response) => {
+  let empDb = req.store.Employees as GenericService<Employee>;
+  let autDb = req.store.Authorities as GenericService<Authority>;
 
-    //departments are no longer stored in the DB
-    // let depDb = req.store.Departments as GenericService<Department>;
+  //departments are no longer stored in the DB
+  // let depDb = req.store.Departments as GenericService<Department>;
 
-    let { id } = req.params;
-    //let item = await empDb.getOne({ ynet_id: id });
+  let { id } = req.params;
+  //let item = await empDb.getOne({ ynet_id: id });
 
-    //if (item) {
-    let item = { authorities: new Array<any>() };
-    let authorities = await autDb.getAll({ "employee.ynet_id": id });
+  //if (item) {
+  let item = { authorities: new Array<any>() };
+  let authorities = await autDb.getAll({ "employee.ynet_id": id });
 
-    if (authorities.length > 0) {
-      let item = _.clone(authorities[authorities.length - 1].employee as any);
-      item.authorities = authorities;
+  if (authorities.length > 0) {
+    let item = _.clone(authorities[authorities.length - 1].employee as any);
+    item.authorities = authorities;
 
-      for (let auth of item.authorities) {
-        // auth.department = await depDb.getOne({ _id: new ObjectId(auth.department_id) });
-
-        /*  if (auth.issue_date)
+    for (let auth of item.authorities) {
+      // auth.department = await depDb.getOne({ _id: new ObjectId(auth.department_id) });
+      /*  if (auth.issue_date)
            auth.issue_date_display = moment(auth.issue_date).format("YYYY-MM-DD"); */
-      }
-      return res.json({ data: item });
     }
+    return res.json({ data: item });
+  }
 
+  //return res.json({ data: item });
+  //}
 
-    //return res.json({ data: item });
-    //}
+  res.status(404).send();
+});
 
-    res.status(404).send();
-  });
+employeeRouter.put("/:id", [param("id").isMongoId()], ReturnValidationErrors, async (req: Request, res: Response) => {
+  let empDb = req.store.Employees as GenericService<Employee>;
 
-employeeRouter.put('/:id',
-  [param("id").isMongoId()], ReturnValidationErrors,
-  async (req: Request, res: Response) => {
-    let empDb = req.store.Employees as GenericService<Employee>;
+  let { id } = req.params;
+  let { first_name, last_name, employee_id, ynet_id, primary_department } = req.body;
+  let item = await empDb.getOne({ _id: new ObjectId(id) });
 
+  if (item) {
+    let update = { first_name, last_name, employee_id, ynet_id, primary_department, email: item.email };
+    empDb.update(id, update);
 
-    let { id } = req.params;
-    let { first_name, last_name, employee_id, ynet_id, primary_department } = req.body;
-    let item = await empDb.getOne({ _id: new ObjectId(id) });
+    return res.json({ data: item });
+  }
 
+  res.status(404).send();
+});
 
-    if (item) {
-      let update = { first_name, last_name, employee_id, ynet_id, primary_department, email: item.email };
-      empDb.update(id, update);
-
-
-      return res.json({ data: item });
-    }
-
-    res.status(404).send();
-  });
-
-employeeRouter.get('/:id/authorities', async (req: Request, res: Response) => {
+employeeRouter.get("/:id/authorities", async (req: Request, res: Response) => {
   let { id } = req.params;
   let db = req.store.Authorities as GenericService<Authority>;
-  let files = await db.getAll({ "employee_id": id });
+  let files = await db.getAll({ employee_id: id });
   return res.json({ data: files });
 });
--- web/src/modules/home/views/Dashboard.vue
+++ web/src/modules/home/views/Dashboard.vue
@@ -1,110 +1,101 @@
 <template>
-    <div class="home">
-        <h1>Signing Authorities Home</h1>
+  <div class="home">
+    <h1>Signing Authorities Home</h1>
 
-        <v-row>
-            <v-col
-                ><v-card class="mt-5" color="#fff2d5">
-                    <v-card-title>Authorities by Employee</v-card-title>
-                    <v-card-text>
-                        <v-text-field
-                            dense
-                            outlined
-                            background-color="white"
-                            label="Search employees"
-                            append-icon="mdi-magnify"
-                            @click:append="doSearch"
-                            @keydown="searchKeyUp"
-                            hint="Enter a Name, YNET ID or Employee ID"
-                            v-model="search"
-                            hide-details
-                        ></v-text-field>
-<!-- 
+    <v-row>
+      <v-col
+        ><v-card class="mt-5" color="#fff2d5">
+          <v-card-title>Authorities by Employee</v-card-title>
+          <v-card-text>
+            <v-text-field
+              dense
+              outlined
+              background-color="white"
+              label="Search employees"
+              append-icon="mdi-magnify"
+              @click:append="doSearch"
+              @keydown="searchKeyUp"
+              hint="Enter a Name, YNET ID or Employee ID"
+              v-model="search"
+              hide-details
+            ></v-text-field>
+            <!-- 
                         <v-card-actions class="mt-2">
                             <router-link to="/search"
                                 >Advanced search</router-link
                             >
                         </v-card-actions> -->
-                    </v-card-text>
-                </v-card></v-col
+          </v-card-text>
+        </v-card></v-col
+      >
+      <v-col>
+        <v-card class="mt-5 pb-2" color="#fff2d5">
+          <v-card-title>Authorities by Department</v-card-title>
+          <v-card-text>
+            <v-autocomplete
+              dense
+              outlined
+              background-color="white"
+              :items="departments"
+              item-text="display_name"
+              item-value="dept"
+              hide-details
+              @change="selectDepartment"
+              :search-input.sync="searchTerm"
+              @keyup.enter="shortCutSelectDepartment(searchTerm)"
             >
-            <v-col>
-                <v-card class="mt-5 pb-2" color="#fff2d5">
-                    <v-card-title>Authorities by Department</v-card-title>
-                    <v-card-text>
-                        <v-autocomplete
-                            dense
-                            outlined
-                            background-color="white"
-                            :items="departments"
-                            item-text="display_name"
-                            item-value="dept"
-                            hide-details
-                            @change="selectDepartment"
-                            :search-input.sync="searchTerm"
-                            @keyup.enter="shortCutSelectDepartment(searchTerm)"
-                        >
-                        </v-autocomplete>
+            </v-autocomplete>
 
-                        <!-- <router-link to="/departments">View all</router-link> -->
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
+            <!-- <router-link to="/departments">View all</router-link> -->
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
-        <!--         <employee-lookup actionName="Authorities" label="" :select="pickPerson"></employee-lookup>
+    <!--         <employee-lookup actionName="Authorities" label="" :select="pickPerson"></employee-lookup>
  -->
-        <v-navigation-drawer
-            v-model="drawer"
-            absolute
-            right
-            temporary
-            width="600"
-            loading
-        >
-            <v-list-item loading>
-                <v-list-item-content>
-                    <v-list-item-title>
-                        <div class="float-right">
-                            <v-btn
-                                x-small
-                                color="primary"
-                                text
-                                :to="'/search?text=' + search"
-                                class="my-0"
-                                style="font-size: 12px !important"
-                            >
-                                Advanced search</v-btn
-                            >
-                        </div>
-                        <div class="float-left">
-                            Employees ({{ searchResults.length }} matches)
-                        </div>
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-
-            <v-divider></v-divider>
-
-            <div style="max-height: 400px; overflow-y: scroll">
-                <v-data-table
-                    hide-default-footer
-                    :headers="[
-                        { text: 'Name', value: 'display_name' },
-                        { text: 'YNET Id', value: 'ynet_id' },
-                        { text: 'Employee Id', value: 'employee_id' },
-                    ]"
-                    :items="searchResults"
-                    :items-per-page="-1"
-                    :loading="loading"
-                    @click:row="selectEmployee"
-                    class="clickable-row"
-                >
-                </v-data-table>
+    <v-navigation-drawer v-model="drawer" absolute right temporary width="600" loading>
+      <v-list-item loading>
+        <v-list-item-content>
+          <v-list-item-title>
+            <div class="float-right">
+              <v-btn
+                x-small
+                color="primary"
+                text
+                :to="'/search?text=' + search"
+                class="my-0"
+                style="font-size: 12px !important"
+              >
+                Advanced search</v-btn
+              >
             </div>
-        </v-navigation-drawer>
-        <!-- repalce with a recent work component -->
-    </div>
+            <div class="float-left">Employees ({{ searchResults.length }} matches)</div>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <div style="max-height: 400px; overflow-y: scroll">
+        <v-data-table
+          hide-default-footer
+          :headers="[
+            { text: 'Name', value: 'display_name' },
+            { text: 'YNET Id', value: 'ynet_id' },
+            { text: 'Authorities', value: 'authority_count' },
+          ]"
+          :items="searchResults"
+          :items-per-page="-1"
+          :loading="loading"
+          @click:row="selectEmployee"
+          class="clickable-row"
+        >
+        </v-data-table>
+      </div>
+    </v-navigation-drawer>
+    <!-- repalce with a recent work component -->
+  </div>
 </template>
 
 <script>
@@ -113,62 +104,58 @@
 import { mapActions, mapState } from "vuex";
 
 export default {
-    name: "Home",
-    //components: { createEmployeeModal, EmployeeLookup },
-    data: () => ({
-        search: "",
-        drawer: null,
-        searchResults: [],
-        loading: false,
-        searchTerm: null,
-    }),
-    computed: {
-        ...mapState("department", ["departments"]),
+  name: "Home",
+  //components: { createEmployeeModal, EmployeeLookup },
+  data: () => ({
+    search: "",
+    drawer: null,
+    searchResults: [],
+    loading: false,
+    searchTerm: null,
+  }),
+  computed: {
+    ...mapState("department", ["departments"]),
+  },
+  methods: {
+    ...mapActions("home", ["employeeSearch"]),
+    searchKeyUp(event) {
+      if (event.key == "Enter") this.doSearch();
     },
-    methods: {
-        ...mapActions("home", ["employeeSearch"]),
-        searchKeyUp(event) {
-            if (event.key == "Enter") this.doSearch();
-        },
-        async doSearch() {
-            let cleanSearch = this.search.trim().toLowerCase();
-            if (cleanSearch.length == 0) return;
+    async doSearch() {
+      let cleanSearch = this.search.trim().toLowerCase();
+      if (cleanSearch.length == 0) return;
 
-            this.loading = true;
+      this.loading = true;
 
-            await this.employeeSearch({ term: cleanSearch })
-                .then((resp) => {
-                    this.searchResults = resp.data.data;
-                    this.drawer = true;
-                    this.resultCount = resp.data.meta.item_count;
-                })
-                .catch((err) => {
-                    this.$emit("showError", err);
-                })
-                .finally(() => {
-                    this.loading = false;
-                });
-        },
-        selectEmployee(item) {
-            this.$router.push(`/employee/${item._id}`);
-        },
-        shortCutSelectDepartment(searchInput) {
-            if (
-                this.departments.filter(
-                    (department) => department.dept === searchInput
-                ).length === 1
-            ) {
-                this.selectDepartment(searchInput);
-            }
-        },
-        selectDepartment(item) {
-            this.$router.push(`/departments/${item}`);
-        },
-        pickPerson(item) {
-            console.log("PICKED", item);
-
-            this.drawer = true;
-        },
+      await this.employeeSearch({ term: cleanSearch })
+        .then((resp) => {
+          this.searchResults = resp.data.data;
+          this.drawer = true;
+          this.resultCount = resp.data.meta.item_count;
+        })
+        .catch((err) => {
+          this.$emit("showError", err);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     },
+    selectEmployee(item) {
+      this.$router.push(`/employee/${item.ynet_id}`);
+    },
+    shortCutSelectDepartment(searchInput) {
+      if (this.departments.filter((department) => department.dept === searchInput).length === 1) {
+        this.selectDepartment(searchInput);
+      }
+    },
+    selectDepartment(item) {
+      this.$router.push(`/departments/${item}`);
+    },
+    pickPerson(item) {
+      console.log("PICKED", item);
+
+      this.drawer = true;
+    },
+  },
 };
 </script>
