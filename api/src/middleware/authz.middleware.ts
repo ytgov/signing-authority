70b047617db8b1d356d4990b60adfa973351d259
Michael Johnson <michael@icefoganalytics.com>
2022-12-01 16:20:55 -0700

    Update authz.middleware.ts

--- api/src/middleware/authz.middleware.ts
+++ api/src/middleware/authz.middleware.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 import jwksRsa from "jwks-rsa";
 import { AUTH0_DOMAIN, AUTH0_AUDIENCE } from "../config";
 import { UserService } from "../services";
+import { ObjectId } from "mongodb";
 
 export const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -103,7 +104,7 @@ export async function loadUser(req: Request, res: Response, next: NextFunction) 
     return next();
   }
 
-  console.log("User not found in DB:", sub, "Looking up userinfo from Auth0")
+  console.log("User not found in DB:", sub, "Looking up userinfo from Auth0");
 
   await axios
     .get(`${AUTH0_DOMAIN}userinfo`, { headers: { authorization: token } })
@@ -121,9 +122,19 @@ export async function loadUser(req: Request, res: Response, next: NextFunction) 
         } else {
           if (!email) email = `${first_name}.${last_name}@yukon-no-email.ca`;
 
-          u = await db.create({ email, sub, status: "Active", first_name, last_name, roles: ["Employee"] });
-          console.log("CREATING USER FOR " + email, sub, u);
-          req.user = { ...req.user, ...u };
+          let eu = await db.getByEmail(email);
+
+          if (eu) {
+            eu.sub = sub;
+            await db.update(eu._id || new ObjectId(), eu);
+
+            console.log("UPDATE USER SUB " + email, sub, u);
+            req.user = { ...req.user, ...eu };
+          } else {
+            u = await db.create({ email, sub, status: "Active", first_name, last_name, roles: ["Employee"] });
+            console.log("CREATING USER FOR " + email, sub, u);
+            req.user = { ...req.user, ...u };
+          }
         }
       } else {
         console.log("Payload from Auth0 is strange or failed for", req.user);
