955f2444f878ec43a47dce02e1ed0b4ad695b709
Michael Johnson <michael@icefoganalytics.com>
2023-03-09 11:53:12 -0700

    Fixing PDF generation margins

--- api/src/utils/pdf-generator.ts
+++ api/src/utils/pdf-generator.ts
@@ -1,12 +1,14 @@
-
 import puppeteer, { PaperFormat } from "puppeteer";
 
-export async function generatePDF(content: string, format: PaperFormat = "letter", landscape: boolean = true): Promise<Buffer> {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--headless', '--disable-gpu'] });
-    const page = await browser.newPage();
-    //await page.setViewport({ width: 1366, height: 768});
-    await page.setContent(content);
-    const pdf = await page.pdf({ format, landscape});
-    await browser.close();
-    return Promise.resolve(pdf);
+export async function generatePDF(
+  content: string,
+  format: PaperFormat = "letter",
+  landscape: boolean = true
+): Promise<Buffer> {
+  const browser = await puppeteer.launch({ args: ["--no-sandbox", "--headless", "--disable-gpu"] });
+  const page = await browser.newPage();
+  await page.setContent(content);
+  const pdf = await page.pdf({ format, landscape, preferCSSPageSize: true });
+  await browser.close();
+  return Promise.resolve(pdf);
 }
