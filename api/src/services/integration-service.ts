7780a76bfeada353aa50421ea1bde18588fd2050
Michael Johnson <michael@icefoganalytics.com>
2023-10-16 10:59:57 -0700

    Update integration-service.ts

--- api/src/services/integration-service.ts
+++ api/src/services/integration-service.ts
@@ -6,20 +6,18 @@ import { cloneDeep } from "lodash";
 
 export class IntegrationService {
   async checkAuthorityChange(baseAuthority: Authority): Promise<any> {
-    console.log("INCOMEING", baseAuthority);
-
     let authority = cloneDeep(baseAuthority);
     let yesterdayAuthority = cloneDeep(baseAuthority);
     setHistoricAuthorityStatus(yesterdayAuthority, moment().subtract(1, "day").toDate());
     setAuthorityStatus(authority);
 
     let email = authority.employee.email;
-    let cancelDate = authority.cancel_date;
     let currentStatus = authority.status;
 
     console.log("checkAuthorityChange", email, authority._id, currentStatus, yesterdayAuthority.status);
 
-    if (currentStatus == "Active " || yesterdayAuthority.status == "Active") {
+    // one must be active, but not both
+    if (currentStatus == "Active" || yesterdayAuthority.status == "Active") {
       if (currentStatus != yesterdayAuthority.status) {
         this.notifyOfAuthorityChange(email);
       }
@@ -28,6 +26,7 @@ export class IntegrationService {
 
   async notifyOfAuthorityChange(email: string): Promise<any> {
     console.log("Sending Authority Change to Integration")
+
     axios
       .get(`${INTEGRATION_ENDPOINT_URL}/${email}`)
       .then((resp) => {
